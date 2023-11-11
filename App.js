7bb9e0e420c29b6910e19b6a4a72a58530bcd9e1
deepanshu19 <dmainh28@gmail.com>
2023-11-11 21:35:37 +0530

    parcel changes

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const heading = React.createElement('h1', { id: 'title' }, 'Heading 1!');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement(
+  'h1',
+  { id: 'title' },
+  'Heading from parcel!'
+);
 
 const heading2 = React.createElement('h2', { id: 'title' }, 'Heading 2!');
 console.log(heading);
